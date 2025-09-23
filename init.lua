91125257c0964f4f2a36173bf927ffd714261273
Vegard B. Bugge <vegabug@live.no>
2025-09-23 02:56:09 +0200

    define spec group for LSP commands

--- init.lua
+++ init.lua
@@ -387,8 +387,9 @@ require('lazy').setup({
 			-- Document existing key chains
 			spec = {
 				{ '<leader>a', group = '[A]I', mode = { 'n', 'v' } },
-				{ '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
 				{ '<leader>g', group = '[G]it' },
+				{ '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+				{ '<leader>l', group = '[L]SP commands' },
 				{ '<leader>s', group = '[S]earch' },
 				{ '<leader>t', group = '[T]oggle' },
 			},
@@ -665,55 +666,67 @@ require('lazy').setup({
 
 					-- Rename the variable under your cursor.
 					-- Most Language Servers support renaming across files
-					map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
+					map('<leader>ln', vim.lsp.buf.rename, 'Re[n]ame symbol')
 
 					-- Execute a code action, usually your cursor needs to be
 					-- on top of an error or a suggestion from your LSP for
 					-- this to activate.
 					map(
-						'gra',
+						'<leader>la',
 						vim.lsp.buf.code_action,
-						'[G]oto Code [A]ction',
+						'Go to Code [A]ction',
 						{ 'n', 'x' }
 					)
 
 					-- Find references for the word under your cursor.
-					map('grr', builtin.lsp_references, '[G]oto [R]eferences')
+					map(
+						'<leader>lr',
+						builtin.lsp_references,
+						'Go to [R]eferences'
+					)
 
 					-- Jump to the implementation of the word under your
 					-- cursor. Useful when your language has ways of declaring
 					-- types without an actual implementation.
 					map(
-						'gri',
+						'<leader>li',
 						builtin.lsp_implementations,
-						'[G]oto [I]mplementation'
+						'Go to [I]mplementation'
 					)
 
 					-- Jump to the definition of the word under your cursor.
 					-- This is where a variable was first declared, or where a
 					-- function is defined, etc. To jump back, press <C-t>.
-					map('grd', builtin.lsp_definitions, '[G]oto [D]efinition')
+					map(
+						'<leader>ld',
+						builtin.lsp_definitions,
+						'[G]oto [D]efinition'
+					)
 
 					-- NB: This is not Goto Definition, this is
 					-- Goto Declaration. For example, in C this would take you
 					-- to the header.
-					map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+					map(
+						'<leader>lp',
+						vim.lsp.buf.declaration,
+						'Go to Declaration/[p]roclamation'
+					)
 
 					-- Fuzzy find all the symbols in your current document.
 					-- Symbols are things like variables, functions, types, etc
 					map(
-						'gO',
+						'<leader>lf',
 						builtin.lsp_document_symbols,
-						'Open Document Symbols'
+						'Open/[f]ind Document Symbols'
 					)
 
 					-- Fuzzy find all the symbols in your current workspace.
 					-- Similar to document symbols, except searches over your
 					-- entire project.
 					map(
-						'gW',
+						'<leader>lw',
 						builtin.lsp_dynamic_workspace_symbols,
-						'Open Workspace Symbols'
+						'Open [W]orkspace Symbols'
 					)
 
 					-- Jump to the type of the word under your cursor. Useful
@@ -721,9 +734,9 @@ require('lazy').setup({
 					-- want to see the definition of its *type*,
 					-- not where it was *defined*.
 					map(
-						'grt',
+						'<leader>lt',
 						builtin.lsp_type_definitions,
-						'[G]oto [T]ype Definition'
+						'Go to [T]ype definition'
 					)
 
 					-- This function resolves a difference between
